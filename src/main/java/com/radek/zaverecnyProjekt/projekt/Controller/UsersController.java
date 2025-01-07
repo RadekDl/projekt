c37875f04a3276f336f48103c39745594bc00c85
RadekDl <radek.dl@atlas.cz>
2025-01-07 21:52:14 +0100

    zaverecny projekt
    úprava portu na 8081
    předělání na jdbc
    oprava načítání z databáze UUID pomocí postman
    rozdělení kódu na service a controler

--- src/main/java/com/radek/zaverecnyProjekt/projekt/Controller/UsersController.java
+++ src/main/java/com/radek/zaverecnyProjekt/projekt/Controller/UsersController.java
@@ -3,16 +3,13 @@ package com.radek.zaverecnyProjekt.projekt.Controller;
 import com.radek.zaverecnyProjekt.projekt.Model.User;
 import com.radek.zaverecnyProjekt.projekt.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.*;
 import java.util.List;
-import java.util.UUID;
+
 
 @RestController
 @RequestMapping ("api/v1")
@@ -20,46 +17,17 @@ import java.util.UUID;
 public class UsersController {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
-
+    UserService userService;
 
 
     @GetMapping("users")
     public List<User> getAllUsers(){
-        List<User> users = jdbcTemplate.query("select * from users;", new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet result, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setID(result.getInt("ID"));
-                        user.setName(result.getString("Name"));
-                        user.setSurname(result.getString("Surname"));
-                        user.setPersonID(result.getString("PersonID"));
-                        String uuidString = result.getString("UUID");
-                        user.setUuid(UUID.fromString(uuidString));
-                        return  user;
-
-                    }
-                });
-
-        return users;
+        return userService.getAllUser();
     }
+
     @GetMapping("users{ID}")
-    public  User getId(@PathVariable("ID")int id){
-        String sql = "select * from users where ID = "+id;
-        User user=jdbcTemplate.queryForObject(sql, new RowMapper <User>() {
-            public User mapRow(ResultSet result, int rowNum)throws SQLException{
-                User user = new User();
-                user.setID(result.getInt("ID"));
-                user.setName(result.getString("Name"));
-                user.setSurname(result.getString("Surname"));
-                user.setPersonID(result.getString("PersonID"));
-                String uuidString = result.getString("UUID");
-                user.setUuid(UUID.fromString(uuidString));
-                return user;
-            }
-        });
-
-        return user;
-
+    public  List<User> getId(@PathVariable("ID")int id){
+        return userService.getId(id);
     }
+
 }
--- src/main/java/com/radek/zaverecnyProjekt/projekt/Service/UserService.java
+++ src/main/java/com/radek/zaverecnyProjekt/projekt/Service/UserService.java
@@ -1,24 +1,73 @@
 package com.radek.zaverecnyProjekt.projekt.Service;
 
 import com.radek.zaverecnyProjekt.projekt.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
-    private List<User> userList = new ArrayList<>();
 
-    public void addUser(User user){
-        userList.add(user);
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<User> getAllUser() {
+        List<User> users = jdbcTemplate.query("select * from users;", new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet result, int rowNum) throws SQLException {
+                User user = new User();
+                user.setID(result.getInt("ID"));
+                user.setName(result.getString("Name"));
+                user.setSurname(result.getString("Surname"));
+                user.setPersonID(result.getString("PersonID"));
+                String uuidString = result.getString("UUID");
+                user.setUuid(UUID.fromString(uuidString));
+                return user;
+
+            }
+        });
+
+        return users;
+
     }
 
-    public List<User> getUserList() {
-        return userList;
-    }
+    public  List<User>  getId(int id) {
+        String sql = "select * from users where ID = " + id;
+        List<User> user = jdbcTemplate.query(sql, new RowMapper<User>() {
+            public User mapRow(ResultSet result, int rowNum) throws SQLException {
+                User user = new User();
+                user.setID(result.getInt("ID"));
+                user.setName(result.getString("Name"));
+                user.setSurname(result.getString("Surname"));
+                user.setPersonID(result.getString("PersonID"));
+                String uuidString = result.getString("UUID");
+                user.setUuid(UUID.fromString(uuidString));
+                return user;
+            }
+        });
 
-    public void setUserList(List<User> userList) {
-        this.userList = userList;
+        return user;
+
+
+//    private List<User> userList = new ArrayList<>();
+//
+//    public void addUser(User user){
+//        userList.add(user);
+//    }
+//
+//    public List<User> getUserList() {
+//        return userList;
+//    }
+//
+//    public void setUserList(List<User> userList) {
+//        this.userList = userList;
+//    }
     }
 }
